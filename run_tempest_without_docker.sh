2b4cf0fe3bed146a5263d44d6d2202b60be7fef1
Oleksiy Butenko <obutenko@mirantis.com>
2016-09-19 20:03:36 +0300

    Update run_tempest_without_docker.sh

--- run_tempest_without_docker.sh
+++ run_tempest_without_docker.sh
@@ -37,9 +37,9 @@ rally verify showconfig
 wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/skip_ceph.list
 wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/skip_lvm.list
 if [ $storage_protocol == 'ceph' ]; then
-    source $CDIR/openrc && rally verify start --skip-list skip_ceph.list > /root/rally/log.log
+    source $CDIR/openrc && rally verify start --regex tempest.api.baremetal --skip-list skip_ceph.list > /root/rally/log.log
 else
-    source $CDIR/openrc && rally verify start --skip-list skip_lvm.list > /root/rally/log.log
+    source $CDIR/openrc && rally verify start --regex tempest.api.baremetal --skip-list skip_lvm.list > /root/rally/log.log
 fi
 
 rally verify results --json --output-file output.json
