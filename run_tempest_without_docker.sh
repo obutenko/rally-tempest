13fc4c02777af26f059d8a20f592fb5598b81760
Oleksiy Butenko <obutenko@mirantis.com>
2016-09-29 13:47:56 +0300

    Update run_tempest_without_docker.sh

--- run_tempest_without_docker.sh
+++ run_tempest_without_docker.sh
@@ -1,45 +1,60 @@
 #!/bin/bash -xe
+
+rm -rf rally .rally /root/openrc_tempest
+cp /root/openrc /root/openrc_tempest
+
+source /root/openrc_tempest && ironic node-create -d fake
+
 apt-get install -y git
-rm -rf rally .rally
+
 git clone https://github.com/openstack/rally.git
 cd rally
 git checkout tags/0.6.0
 CDIR=$(pwd)
-echo $CDIR
-cp /root/openrc $CDIR
-sed -i 's|:5000|:5000/v2.0|g' openrc
-IS_TLS=$(source /root/openrc; openstack endpoint show identity 2>/dev/null | awk '/https/')
+
+IS_TLS=$(source /root/openrc_tempest; openstack endpoint show identity 2>/dev/null | awk '/https/')
     if [ "${IS_TLS}" ]; then
-        cp /var/lib/astute/haproxy/public_haproxy.pem $CDIR
-        echo "export OS_CACERT='$CDIR/public_haproxy.pem'" >> $CDIR/openrc
+        echo "export OS_CACERT='/var/lib/astute/haproxy/public_haproxy.pem'" >> /root/openrc_tempest
     fi
-./install_rally.sh -y
-source openrc
-sed -i 's|#swift_operator_role = Member|swift_operator_role = SwiftOperator|g' /etc/rally/rally.conf
-wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/lvm
-wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/ceph
-storage_protocol="lvm"
+
+./install_rally.sh -d rally-venv/ -y
+
+sed -i 's|#swift_operator_role = Member|swift_operator_role = SwiftOperator|g' /root/rally/rally-venv/etc/rally/rally.conf
+
+NOVA_FLTR=$(sed -n '/scheduler_default_filters=/p' /etc/nova/nova.conf | cut -f2 -d=)
 check_ceph=$(cat /etc/cinder/cinder.conf |grep '\[RBD-backend\]' | wc -l)
 if [ ${check_ceph} == '1' ]; then
     storage_protocol="ceph"
+    wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/skip_ceph.list
+    wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/ceph
+    echo 'scheduler_available_filters = '$NOVA_FLTR >> ceph
+else
+    storage_protocol="lvm"
+    wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/skip_lvm.list
+    wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/lvm
+    echo 'scheduler_available_filters = '$NOVA_FLTR >> lvm
 fi
 
-NOVA_FLTR=$(sed -n '/scheduler_default_filters=/p' /etc/nova/nova.conf | cut -f2 -d=)
+source /root/rally/rally-venv/bin/activate
+source /root/openrc_tempest
 
-echo 'FILTERS='$NOVA_FLTR >> lvm
-echo 'FILTERS='$NOVA_FLTR >> ceph
+apt install -y jq
+rm -rf /root/existing-keystone-v3.json /root/keystone-v3.json
+wget -P /root/ https://raw.githubusercontent.com/openstack/rally/master/samples/deployments/existing-keystone-v3.json
+
+jq ". + { \"auth_url\": \"${OS_AUTH_URL}v3/\" } + { \"admin\" : {\"username\": \"admin\",\"password\": \"admin\", \"project_name\": \"admin\", \"project_domain_name\": \"Default\", \"user_domain_name\": \"Default\", \"admin_domain_name\": \"Default\"}}" /root/existing-keystone-v3.json > /root/keystone-v3.json
+cat /root/keystone-v3.json
+
 rally-manage db recreate
-rally deployment create --fromenv --name=tempest 
+rally deployment create --name=tempest --file=/home/rally/keystone-v3.json
 rally verify install
-rally verify genconfig --add-options $storage_protocol 
+rally verify genconfig --add-options $storage_protocol
 rally verify showconfig
 
-wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/skip_ceph.list
-wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/skip_lvm.list
 if [ $storage_protocol == 'ceph' ]; then
-    source $CDIR/openrc && rally verify start --regex tempest.api.baremetal --skip-list skip_ceph.list > /root/rally/log.log
+    rally verify start --regex tempest.api.keystone --skip-list skip_ceph.list > /root/rally/log.log
 else
-    source $CDIR/openrc && rally verify start --regex tempest.api.baremetal --skip-list skip_lvm.list > /root/rally/log.log
+    rally verify start --regex tempest.api.keystone --skip-list skip_lvm.list > /root/rally/log.log
 fi
 
 rally verify results --json --output-file output.json
@@ -47,3 +62,5 @@ rally verify showconfig > /root/rally/tempest.conf
 cp $(find / -name tempest.log) /root/rally/tempest.log
 git clone https://github.com/greatehop/rally_json2junit
 python rally_json2junit/rally_json2junit/results_parser.py output.json
+
+deactivate
